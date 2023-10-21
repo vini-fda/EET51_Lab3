1cd8db510cc68390939ba3a224409d883b56d237
Vinicius <vinifreitas.d.a@gmail.com>
2023-10-21 01:29:12 -0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,206 @@
+use image::GrayImage;
+
+/*
+    The Golomb encoding algorithm is a lossless data compression method used for entropy encoding. 
+    It’s particularly effective when applied to data that exhibit geometrically distributed characteristics.
+    Below is a straightforward explanation of the algorithm:
+
+    1. **Parameter Selection:**
+       - Choose a positive integer parameter `M`. The choice of `M` affects the efficiency of the encoding.
+       - `M` should be chosen based on the characteristics of the input data.
+       - In practice, it is convenient to choose `M` as a power of 2.
+       - In this case, we will use `k` as the smallest integer greater than or equal to log2(mu/2), 
+         where `mu` is the mean of the input data, and `M = 2^k`
+
+    2. **Encoding a Non-negative Integer:**
+       - Divide the integer `x` by the parameter `M`, obtaining a quotient `q` and a remainder `r`.
+       - `q` is the result of the integer division, and `r` is the remainder: `x = q * M + r`
+
+    3. **Unary Coding of the Quotient:**
+       - Encode the quotient `q` in unary code. Unary coding involves writing `q` zeroes followed by a one: `0^q 1`
+
+    4. **Binary Coding of the Remainder:**
+       - Encode the remainder `r` in binary. The binary code can be of fixed or variable length depending on the chosen `M`.
+       - If `M` is a power of 2, simple binary encoding is used. Otherwise, some variations like truncated binary encoding may be applied.
+
+    5. **Concatenation:**
+       - Concatenate the unary code of `q` and the binary code of `r` to get the final Golomb code.
+
+    Example:
+        Suppose we choose `M = 3` and we want to encode the number `10`.
+        - Quotient and Remainder: `10 = 3 * 3 + 1`
+        - Unary code of quotient: `0001`
+        - Binary code of remainder: `01`
+        - Concatenated Golomb code: `000101`
+
+    Decoding involves reversing these steps, extracting the quotient and remainder from the encoded bitstream, 
+    and reconstructing the original integer using the formula `x = q * M + r`.
+
+    Note: Golomb coding is efficient for data where lower values are more probable than higher values, 
+    making it suitable for applications like image compression and run-length encoding.
+*/
+pub fn golomb_encode(data: &[u8]) -> (u8, Vec<u8>) {
+    let mut mean = 0.0;
+    for &n in data {
+        mean += n as f32;
+    }
+    mean /= data.len() as f32;
+
+    let mut m = 1u8;
+    while (m as f32) < mean / 2.0 {
+        m *= 2;
+    }
+
+    (m, golomb_encode_inner(data, m))
+}
+
+
+fn golomb_encode_inner(data: &[u8], m: u8) -> Vec<u8> {
+    let mut encoded_bits: Vec<u8> = Vec::new();
+    // we assume m is a power of 2
+    let mut b = 0;
+    while (1 << b) < m {
+        b += 1;
+    }
+
+    for &n in data {
+        // Quotient and Remainder Calculation
+        let q = n / m;
+        let r = n % m;
+
+        // Unary Encoding of the Quotient
+        // add q values of 0
+        encoded_bits.resize(encoded_bits.len() + q as usize, 0);
+        // add a 1
+        encoded_bits.push(1);
+
+        // Truncated Binary Encoding of the Remainder
+        if r < m {
+            for i in (0..b).rev() {
+                encoded_bits.push((r >> i) & 1);
+            }
+        } else {
+            let adjusted_r = r + m;
+            for i in (0..=b).rev() {
+                encoded_bits.push((adjusted_r >> i) & 1);
+            }
+        }
+    }
+    println!("Number of bits: {}", encoded_bits.len());
+    pack_bits(&encoded_bits)
+}
+
+pub fn print_as_bits(data: &[u8]) {
+    for &bit in data {
+        print!("{}", bit);
+    }
+    println!();
+}
+
+fn pack_bits(encoded_bits: &[u8]) -> Vec<u8> {
+    let mut packed_bytes = Vec::new();
+    let mut current_byte = 0u8;
+    let mut bit_count = 0;
+
+    for bit in encoded_bits {
+        current_byte = (current_byte << 1) | bit;
+        bit_count += 1;
+
+        if bit_count == 8 {
+            packed_bytes.push(current_byte);
+            current_byte = 0;
+            bit_count = 0;
+        }
+    }
+
+    // If there are remaining bits, pad and push them
+    if bit_count != 0 {
+        current_byte <<= 8 - bit_count;
+        packed_bytes.push(current_byte);
+    }
+
+    packed_bytes
+}
+
+/// Convert image to bytes
+pub fn image_to_bytes(image: &GrayImage) -> Vec<u8> {
+    image.pixels().map(|p| p[0]).collect()
+}
+
+/// Convert bytes to image
+pub fn bytes_to_image(bytes: &[u8], width: u32, height: u32) -> GrayImage {
+    let mut image = GrayImage::new(width, height);
+    for (i, pixel) in image.pixels_mut().enumerate() {
+        pixel[0] = bytes[i];
+    }
+    image
+}
+
+pub fn golomb_decode(m: u8, data: &[u8]) -> Vec<u8> {
+    let mut decoded_bits: Vec<u8> = Vec::new();
+    // we assume m is a power of 2
+    let mut b = 0;
+    while (1 << b) < m {
+        b += 1;
+    }
+
+    let mut bit_index = 0;
+    while bit_index < data.len() * 8 {
+        // Unary Decoding of the Quotient
+        let mut q = 0;
+        while data[bit_index / 8] & (1 << (7 - (bit_index % 8))) == 0 {
+            q += 1;
+            bit_index += 1;
+            if bit_index >= data.len() * 8 {
+                return decoded_bits;
+            }
+        }
+        bit_index += 1;
+
+        // Truncated Binary Decoding of the Remainder
+        let mut r = 0;
+        for _ in 0..b {
+            r <<= 1;
+            if data[bit_index / 8] & (1 << (7 - (bit_index % 8))) != 0 {
+                r |= 1;
+            }
+            bit_index += 1;
+            if bit_index >= data.len() * 8 {
+                return decoded_bits;
+            }
+        }
+
+        // Reconstruct the original integer
+        let n = q * m + r;
+        decoded_bits.push(n);
+    }
+
+    decoded_bits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_golomb_encode() {
+        let data = vec![21, 21, 21, 21];
+        let encoded = golomb_encode_inner(&data, 8);
+        // The expected binary sequence for 21 is 001101
+        // So, the packed bytes should be 001101|001101|001101|001101 = 00110100 11010011 01001101
+        let expected = vec![0b00110100, 0b11010011, 0b01001101];
+
+        assert_eq!(encoded, expected, "Golomb encoding failed to provide the expected output.");
+    }
+
+    #[test]
+    fn test_golomb_encode_2() {
+        let data = vec![21];
+        let encoded = golomb_encode_inner(&data, 8);
+        // The expected binary sequence for 21 is 001101
+        // So, the packed bytes should be 001101 -> 00110100
+        let expected = vec![0b00110100];
+
+        assert_eq!(encoded, expected, "Golomb encoding failed to provide the expected output.");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,253 @@
+use std::collections::BTreeMap;
+use eet51_lab3::{golomb_encode, image_to_bytes, golomb_decode, bytes_to_image};
+use image::{buffer::Pixels, Luma, GrayImage};
+use serde::Serialize;
+use ndarray::Array2;
+use std::env;
+
+fn grayscale_histogram(pixels: Pixels<'_, Luma<u8>>) -> BTreeMap<u8, u32> {
+    let mut histogram = BTreeMap::new();
+    // 0 - 255
+    for i in 0..=255 {
+        histogram.insert(i, 0);
+    }
+    for pixel in pixels {
+        let count = histogram.entry(pixel.0[0]).or_insert(0);
+        *count += 1;
+    }
+    histogram
+}
+
+fn matrix_histogram(matrix: &Array2<i32>) -> BTreeMap<i32, u32> {
+    let mut histogram = BTreeMap::new();
+    for pixel in matrix.iter() {
+        let count = histogram.entry(*pixel).or_insert(0);
+        *count += 1;
+    }
+    histogram
+}
+
+#[derive(Serialize)]
+struct PixelFrequency {
+    pixel: u8,
+    frequency: f32,
+}
+
+fn save_to_csv(histogram: &BTreeMap<u8, f32>, filename: &str) {
+    let mut wtr = csv::Writer::from_path(filename).unwrap();
+    for (key, value) in histogram.iter() {
+        wtr.serialize(PixelFrequency {
+            pixel: *key,
+            frequency: *value,
+        }).unwrap();
+    }
+    wtr.flush().unwrap();
+}
+
+fn calculate_entropy<T>(histogram: &BTreeMap<T, f32>) -> f32 {
+    let mut entropy = 0.0;
+    for (_, &value) in histogram.iter() {
+        if value > 0.0 {
+            entropy -= value * value.log2();
+        }
+    }
+    entropy
+}
+
+// create a new image with the same dimensions as the original
+// for each pixel in the original image, find the corresponding pixel in the new image
+// output(i, j) = input(i,j) -input(i-1,j) - input(i,j-1) + input(i-1,j-1)
+fn prediction_err_matrix(original: &GrayImage) -> Array2<i32> {
+    // take the dimensions (u32, u32) and convert to (usize, usize)
+    let (width, height) = original.dimensions();
+    let width = width as usize;
+    let height = height as usize;
+    let mut new_matrix = Array2::zeros((width, height));
+
+    for x in 0..width {
+        for y in 0..height {
+            let new_value;
+            let f = |x: usize, y: usize| -> i32 {
+                if x >= width || y >= height {
+                    0
+                } else {
+                    original.get_pixel(x as u32, y as u32)[0] as i32
+                }
+            };
+            if x > 0 && y > 0 {
+                new_value = f(x, y) - f(x - 1, y) - f(x, y - 1) + f(x - 1, y - 1);
+            } else if x > 0 {
+                new_value = f(x, y) - f(x - 1, y);
+            } else if y > 0 {
+                new_value = f(x, y) - f(x, y - 1);
+            } else {
+                new_value = f(x, y);
+            }
+            new_matrix[[x, y]] = new_value;
+        }
+    }
+    new_matrix
+}
+
+fn reconstruct_image_from_pred_err_matrix(matrix: &Array2<i32>) -> GrayImage {
+    let (width, height) = matrix.dim();
+    let mut reconstructed_image = GrayImage::new(width as u32, height as u32);
+    
+    let f = |x: usize, y: usize| -> i32 {
+        if x >= width || y >= height {
+            0
+        } else {
+            matrix[[x, y]]
+        }
+    };
+
+
+
+    for x in 0..width {
+        for y in 0..height {
+            let mut new_pixel;
+
+            let g = |x: usize, y: usize| -> i32 {
+                if x >= width || y >= height {
+                    0
+                } else {
+                    reconstructed_image.get_pixel(x as u32, y as u32)[0] as i32
+                }
+            };
+
+            // I(x,y) = \sum_{m=0}^{x} \sum_{n=0}^{y} P(m,n)
+            // => I(x,y) = I(x-1,y) + I(x,y-1) - I(x-1,y-1) + P(x,y)
+            if x > 0 && y > 0 {
+                new_pixel = g(x - 1, y) + g(x, y - 1) - g(x - 1, y - 1) + f(x, y);
+            } else if x > 0 {
+                new_pixel = g(x - 1, y) + f(x, y);
+            } else if y > 0 {
+                new_pixel = g(x, y - 1) + f(x, y);
+            } else {
+                new_pixel = f(x, y);
+            }
+            //cap the value at 0 and 255
+            if new_pixel < 0 {
+                new_pixel = 0;
+            } else if new_pixel > 255 {
+                new_pixel = 255;
+            }
+            reconstructed_image.put_pixel(x as u32, y as u32, image::Luma([new_pixel as u8]));
+        }
+    }
+    reconstructed_image
+}
+
+fn convert_to_grayscale_image(matrix: &Array2<i32>) -> GrayImage {
+    let (width, height) = matrix.dim();
+    let mut new_image = GrayImage::new(width as u32, height as u32);
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = matrix[[x, y]];
+            //cap the value at 0 and 255
+            let pixel = if pixel < 0 {
+                0u8
+            } else if pixel > 255 {
+                255u8
+            } else {
+                pixel as u8
+            };
+            new_image.put_pixel(x as u32, y as u32, image::Luma([pixel]));
+        }
+    }
+    new_image
+}
+
+fn complete_tasks(img: &GrayImage, img_name: &str) {
+    // Task A (Item 2): calculate the relative frequency of each pixel value in the image
+    let histogram = grayscale_histogram(img.pixels());
+    let mut relative_freq = BTreeMap::new();
+    for (&key, &value) in histogram.iter() {
+        relative_freq.insert(key, value as f32 / (img.width() * img.height()) as f32);
+    }
+    let filename = format!("{}.csv", img_name);
+    save_to_csv(&relative_freq, &filename);
+    // Task B (Item 3): calculate the entropy of a pixel modeled as a random variable
+    let entropy = calculate_entropy(&relative_freq);
+    println!("Entropy: {}", entropy);
+    // Task C (Item 4): calculate the prediction error matrix
+    let prediction_err = prediction_err_matrix(img);
+    println!("Prediction error matrix: {:?}", prediction_err);
+
+    // Task D (Item 5): calculate the entropy of the prediction error matrix
+    let matrix_histogram = matrix_histogram(&prediction_err);
+    let mut matrix_relative_freq = BTreeMap::new();
+    for (&key, &value) in matrix_histogram.iter() {
+        matrix_relative_freq.insert(key, value as f32 / (img.width() * img.height()) as f32);
+    }
+    let matrix_entropy = calculate_entropy(&matrix_relative_freq);
+    println!("Prediction error matrix entropy: {}", matrix_entropy);
+
+    // Task E (Item 6): reconstruct the image from the prediction error matrix
+    let reconstructed_image = reconstruct_image_from_pred_err_matrix(&prediction_err);
+    //verify that they are equal
+    verify_equality_imgs(img, &reconstructed_image);
+
+    // Task F (Item 7): create a new image from the absolute value of the prediction error matrix
+    let abs_prediction_err = prediction_err.mapv(|x| x.abs());
+    let abs_reconstructed_image = convert_to_grayscale_image(&abs_prediction_err);
+
+    // Task G (Item 8): calculate the entropy of the absolute value of the prediction error matrix
+    let abs_histogram = grayscale_histogram(abs_reconstructed_image.pixels());
+    let mut abs_relative_freq = BTreeMap::new();
+    for (&key, &value) in abs_histogram.iter() {
+        abs_relative_freq.insert(key, value as f32 / (img.width() * img.height()) as f32);
+    }
+    let abs_entropy = calculate_entropy(&abs_relative_freq);
+    println!("Absolute entropy: {}", abs_entropy);
+
+    // Task H (Item 9): Use the Golomb encoding function to encode the absolute value of the prediction error matrix
+    let abs_bytes = image_to_bytes(&abs_reconstructed_image);
+    let (m, encoded) = golomb_encode(&abs_bytes);
+    // print the difference in bytes
+    println!("Original image size: {} bytes", abs_bytes.len());
+    println!("Encoded image size: {} bytes", encoded.len());
+    // Compression ratio
+    println!("Compression ratio: {}", abs_bytes.len() as f32 / encoded.len() as f32);
+    // Percentage ratio of image size
+    println!("Percentage of original image size: {:.2}%", encoded.len() as f32 / abs_bytes.len() as f32 * 100.0);
+
+    // This is not a task, but we will decode the encoded bytes and verify that they are equal
+    let decoded = golomb_decode(m, &encoded);
+    let decoded_image = bytes_to_image(&decoded, img.width(), img.height());
+    verify_equality_imgs(&abs_reconstructed_image, &decoded_image);
+
+}
+
+/// Verify that two grayscale images are equal
+/// 1. They must have the same dimensions
+/// 2. Each pixel must have the same value
+/// 
+/// We show the exact point where the images differ
+/// with useful error messages
+fn verify_equality_imgs(a: &GrayImage, b: &GrayImage) {
+    if a.dimensions() != b.dimensions() {
+        panic!("Images are not equal: dimensions are different");
+    }
+    for (x, y, pixel) in a.enumerate_pixels() {
+        if pixel[0] != b.get_pixel(x, y)[0] {
+            panic!("Images are not equal at ({}, {}): {} != {}", x, y, pixel[0], b.get_pixel(x, y)[0]);
+        }
+    }
+    println!("Images are equal");
+}
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Please provide the path to the image.");
+        return;
+    }
+
+    let img_path = &args[1];
+    let img = image::open(img_path).unwrap().to_luma8();
+    let img_name = img_path.split('/').last().unwrap().split('.').next().unwrap();
+    complete_tasks(&img, img_name);
+}
